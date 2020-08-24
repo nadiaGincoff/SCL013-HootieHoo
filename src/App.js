c9e8569d20b23c16c497001ad389bef744422afb
AnlianaAguilar <anlianaaguilar@gmail.com>
2020-08-24 09:50:37 -0400

    Ruteo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Niños from './Components/Niños'
+import Inicio from './Components/Inicio'
+import Adulto from './Components/Adultos'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Switch>
+          <Route path="/Niños">
+            <Niños/>    
+          </Route>
+          <Route path="/Adulto">
+            <Adulto/>
+          </Route>
+          <Route path="/">
+            <Inicio/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
