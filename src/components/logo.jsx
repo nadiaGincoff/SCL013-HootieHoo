5b52fb481d56db4aea3737a36365a340f878b9ef
ariela tolosa <ariela.tolosa.silva@gmail.com>
2020-08-24 09:54:55 -0400

    prueba

---
+++ src/components/logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/img/logo.png'
+
+const logo = () => {
+    return (
+        <div>
+            <img src={logo} alt="user pic"/>
+        </div>
+    )
+}
+
+export default logo
